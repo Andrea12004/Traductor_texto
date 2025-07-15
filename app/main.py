220a3ac6c5e8851de635cf98da558e5565a3476a
Andrea12004 <cardonasernaandrea@gmail.com>
2025-07-15 14:02:16 -0500

    Traductor_texto

--- app/main.py
+++ app/main.py
@@ -1,5 +1,20 @@
-from fastapi.responses import FileResponse
-import os
+﻿from fastapi import FastAPI
+from fastapi.responses import FileResponse 
+from pydantic import BaseModel
+import os 
+
+from app.procesador.traductor import procesar_frase, obtener_rutas_videos
+
+app = FastAPI()
+
+class Frase(BaseModel):
+    texto: str
+
+@app.post("/procesar")
+def procesar(frase: Frase):
+    video_ids = procesar_frase(frase.texto)
+    videos = obtener_rutas_videos(video_ids)
+    return {"videos": videos}
 
 @app.get("/video/{video_id}")
 def get_video(video_id: str):
@@ -7,3 +22,9 @@ def get_video(video_id: str):
     if not os.path.exists(video_path):
         return {"error": "Video no encontrado"}
     return FileResponse(video_path, media_type="video/mp4")
+
+
+@app.get("/listar_videos")
+def listar_videos():
+    archivos = os.listdir("videos")
+    return {"videos_disponibles": archivos}
